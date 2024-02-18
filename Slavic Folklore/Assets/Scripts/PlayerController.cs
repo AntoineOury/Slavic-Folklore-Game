0d924e58140ede0f49845a52610b9eed6ff80fb9
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-02-18 14:32:17 +0000

    Some player controller adjustments with ChatGPT

--- Slavic Folklore/Assets/Scripts/PlayerController.cs
+++ Slavic Folklore/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float speed = 5f;
-    private float moveHorizontal, moveVertical;
+    // [SerializeField] private Rigidbody2D rb;
+    // [SerializeField] private float speed = 5f;
+    // private float moveHorizontal, moveVertical;
+    //
+    // private void Awake()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
+    // }
+    //
+    // private void FixedUpdate()
+    // {
+    //     moveHorizontal = Input.GetAxis("Horizontal") * speed;
+    //     moveVertical = Input.GetAxis("Vertical") * speed;
+    //     rb.velocity = new Vector2(moveHorizontal, moveVertical);
+    // }
+    
 
-    private void Awake()
+    //ChatGPT fixed code (above code has player move diagonally):
+    public float moveSpeed = 3f;
+    
+    void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+    
+        Vector2 inputDirection = new Vector2(horizontalInput, verticalInput).normalized;
+        Vector2 movement = inputDirection * moveSpeed * Time.deltaTime;
+    
+        transform.Translate(movement);
     }
 
-    private void FixedUpdate()
-    {
-        moveHorizontal = Input.GetAxis("Horizontal") * speed;
-        moveVertical = Input.GetAxis("Vertical") * speed;
-        rb.velocity = new Vector2(moveHorizontal, moveVertical);
-    }
+
     
     
 }
