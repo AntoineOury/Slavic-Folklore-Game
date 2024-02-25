4d5c891a223b8b304c9b3b3f6e24a55543b6d165
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-02-25 17:45:23 +0000

    Player controller script
    Movement along the XZ axis instead of XY. Slightly slow on the X axis compared to Z although same speed variable is passed.... To be fixed?

--- Slavic Folklore/Assets/Scripts/PlayerController.cs
+++ Slavic Folklore/Assets/Scripts/PlayerController.cs
@@ -5,38 +5,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // [SerializeField] private Rigidbody2D rb;
-    // [SerializeField] private float speed = 5f;
-    // private float moveHorizontal, moveVertical;
+  
+  //Code written with help from ChatGPT and Unity Forum posts: 
+  //https://discussions.unity.com/t/object-moving-on-xy-instead-of-xz/244892
+  //https://forum.unity.com/threads/unity-input-system-move-along-z-axis.961165/
+  
+  public float moveSpeed = 5f;
+
+  private Rigidbody2D rb;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void Update()
+  {
+    // Player movement along the X axis
+    float moveX = Input.GetAxis("Horizontal");
+
+    // Player movement along the Z axis
+    float moveZ = Input.GetAxis("Depth");
+
+    // Calculate movement vector
+    // Vector2 movement = new Vector2(moveX, 0f) * moveSpeed * Time.deltaTime;
     //
-    // private void Awake()
-    // {
-    //     rb = GetComponent<Rigidbody2D>();
-    // }
-    //
-    // private void FixedUpdate()
-    // {
-    //     moveHorizontal = Input.GetAxis("Horizontal") * speed;
-    //     moveVertical = Input.GetAxis("Vertical") * speed;
-    //     rb.velocity = new Vector2(moveHorizontal, moveVertical);
-    // }
+    // transform.position += new Vector3(0f, 0f, moveZ) * moveSpeed * Time.deltaTime;
     
-
-    //ChatGPT fixed code (above code has player move diagonally):
-    public float moveSpeed = 3f;
-    
-    void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-    
-        Vector2 inputDirection = new Vector2(horizontalInput, verticalInput).normalized;
-        Vector2 movement = inputDirection * moveSpeed * Time.deltaTime;
-    
-        transform.Translate(movement);
-    }
+    Vector2 horizontalMovement = new Vector2(moveX, 0f) * moveSpeed * Time.deltaTime;
+    Vector3 depthMovement = new Vector3(0f, 0f, moveZ) * moveSpeed * Time.deltaTime;
 
 
+
+    // Apply movement
+    rb.MovePosition(rb.position + horizontalMovement);
+    transform.position += depthMovement;
     
-    
+  }
+  
 }
