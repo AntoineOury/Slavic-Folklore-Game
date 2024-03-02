133baa05c8a8518539c38311e42f12a70fc2b6f5
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-03-02 17:34:15 +0000

    Notebook Pages ScriptableObject pt1

---
+++ Slavic Folklore/Assets/ScriptableObjects/EntitiesSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntitiesSheet", menuName = "EntitiesPage")]
+public class EntitiesSO : ScriptableObject
+{
+   public new string name;
+   public string partialInfo;
+   public string fullInfo;
+
+   //artwork of the entity before the quest for it has been completed and after
+   public Sprite artShadow;
+   public Sprite entityArt;
+   
+   //check if quest for that entity has been completed so that it can replace the partialInfo with the fullInfo
+}
