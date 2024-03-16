6c2a87b072abf890808e9b6d3330cfd4773f8eff
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-03-16 18:13:25 +0000

    Diary UI interaction pt.1

--- Slavic Folklore/Assets/Scripts/EntitySheet.cs
+++ Slavic Folklore/Assets/Scripts/EntitySheet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.UI;
 public class EntitySheet : MonoBehaviour
 {
     //plug in the relevant SO here 
@@ -10,10 +11,18 @@ public class EntitySheet : MonoBehaviour
     
     //list the UI elements that we're gonna want to alter via SO
     public TMP_Text basicInfo;
+    public TMP_Text entityName;
+
+    public Image entityImage;
     
     void Start()
     {
+        entityName.text = relevantSO.name;
         basicInfo.text = relevantSO.partialInfo;
+        entityImage.sprite = relevantSO.artShadow;
+
+
+
     }
     
 }
