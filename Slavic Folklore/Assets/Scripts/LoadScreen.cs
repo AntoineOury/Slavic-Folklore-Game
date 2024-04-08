c61e93b339f0b2c720cb2f11aee00362fdbec1a5
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-04-08 17:43:21 +0100

    Loading Scene + scene switch script

---
+++ Slavic Folklore/Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScreen : MonoBehaviour
+{
+    //code following: https://www.youtube.com/watch?v=wvXDCPLO7T0
+    
+    public GameObject LoadingScreen;
+
+    public Image LoadingBarFill;
+
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        LoadingScreen.SetActive(true);
+        
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            LoadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
