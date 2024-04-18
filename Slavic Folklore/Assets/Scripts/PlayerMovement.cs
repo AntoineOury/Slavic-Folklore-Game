3fae3435e5bebd7dab4ccdcba761a5f024edb3b4
JM8-art <jakub.mazurek74@gmail.com>
2024-04-18 21:08:08 +0100

    Added Animator - issue with flipping!

--- Slavic Folklore/Assets/Scripts/PlayerMovement.cs
+++ Slavic Folklore/Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
     
     //Code from this amazing tutorial: https://youtu.be/cqNBA9Pslg8?si=9bRwbo8D6tvZeWQo
-    
-    
-    public float speed;
-
-    public float groundDist;
-
-    public LayerMask terrainLayer;
-
-    public Rigidbody rb;
-
-    public SpriteRenderer sr;
-    
-    
-    // Start is called before the first frame update
-    void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+    public float speed;
+    public float groundDist;
+    public LayerMask terrainLayer;
+    public Rigidbody rb;
+    public Animator animator; // Add Animator variable
+    public GameObject walkingSprite; // Reference to the GameObject containing the walking sprite
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>(); // Get the Animator component
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         RaycastHit hit;
         Vector3 castPos = transform.position;
@@ -47,13 +40,16 @@ public class PlayerMovement : MonoBehaviour
         Vector3 moveDir = new Vector3(x, 0, y);
         rb.velocity = moveDir * speed;
 
-        if (x != 0 && x < 0)
+        animator.SetBool("isWalking", moveDir.magnitude > 0); // Set isWalking based on movement magnitude
+        
+        if (moveDir.x < 0)
         {
-            sr.flipX = true;
+            walkingSprite.transform.localScale = new Vector3(-1, 1, 1); // Flip sprite if moving left
         }
-        else if (x != 0 && x > 0)
+        else if (moveDir.x > 0)
         {
-            sr.flipX = false;
+            walkingSprite.transform.localScale = new Vector3(1, 1, 1); // Reset scale if moving right
         }
     }
 }
+
