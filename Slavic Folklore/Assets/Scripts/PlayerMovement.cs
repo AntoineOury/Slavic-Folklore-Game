1efc885e4807bf3b0445e1ecda09c582c197adfb
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-04-20 13:33:24 +0100

    Revert "Added Animator - issue with flipping!"
    This reverts commit 3fae3435e5bebd7dab4ccdcba761a5f024edb3b4.

--- Slavic Folklore/Assets/Scripts/PlayerMovement.cs
+++ Slavic Folklore/Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+{
     
     //Code from this amazing tutorial: https://youtu.be/cqNBA9Pslg8?si=9bRwbo8D6tvZeWQo
-    {
+    
+    
     public float speed;
-    public float groundDist;
-    public LayerMask terrainLayer;
-    public Rigidbody rb;
-    public Animator animator; // Add Animator variable
-    public GameObject walkingSprite; // Reference to the GameObject containing the walking sprite
 
-    private void Start()
+    public float groundDist;
+
+    public LayerMask terrainLayer;
+
+    public Rigidbody rb;
+
+    public SpriteRenderer sr;
+    
+    
+    // Start is called before the first frame update
+    void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>(); // Get the Animator component
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
         RaycastHit hit;
         Vector3 castPos = transform.position;
@@ -40,16 +47,13 @@ public class PlayerMovement : MonoBehaviour
         Vector3 moveDir = new Vector3(x, 0, y);
         rb.velocity = moveDir * speed;
 
-        animator.SetBool("isWalking", moveDir.magnitude > 0); // Set isWalking based on movement magnitude
-        
-        if (moveDir.x < 0)
+        if (x != 0 && x < 0)
         {
-            walkingSprite.transform.localScale = new Vector3(-1, 1, 1); // Flip sprite if moving left
+            sr.flipX = true;
         }
-        else if (moveDir.x > 0)
+        else if (x != 0 && x > 0)
         {
-            walkingSprite.transform.localScale = new Vector3(1, 1, 1); // Reset scale if moving right
+            sr.flipX = false;
         }
     }
 }
-
