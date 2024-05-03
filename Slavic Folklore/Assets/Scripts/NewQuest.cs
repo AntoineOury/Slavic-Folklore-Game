670d5322a6b32c2da753a902917eefb476963507
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-05-03 20:50:01 +0100

    New quest script and animation pt2

--- Slavic Folklore/Assets/Scripts/NewQuest.cs
+++ Slavic Folklore/Assets/Scripts/NewQuest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,21 +9,40 @@ using UnityEngine.UI;
 
 public class NewQuest : MonoBehaviour
 {
+    [Tooltip("QuestSO that is relevant to the unlocked quest")]
     public QuestSO relevantSO;
     
     public GameObject newQuest;
     public TMP_Text questTitle;
 
-    public Animator questText;
+    [Tooltip("drag in the Quest Title text object")]
+    public Animator questTextAnim;
 
     public Button acceptQuest;
 
+    [Tooltip("drag MainCamera in")]
     public PostProcessVolume ppVol;
+
+    [Tooltip("drag Player in")]
+    public PlayerMovement playermovement;
     
+    //hide those menu buttons
+    public GameObject mainButtons;
+    
+
+    private void Start()
+    {
+        //get the player movement script
+        playermovement = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            //lock player in place
+            playermovement.speed = 0f;
+            
             StartCoroutine(QuestAnim());
             
         }
@@ -32,11 +52,13 @@ public class NewQuest : MonoBehaviour
    {
        newQuest.gameObject.SetActive(true);
        questTitle.text = relevantSO.questName;
+       mainButtons.gameObject.SetActive(false);
        ppVol.enabled = true;
        
        yield return new WaitForSeconds(3);
 
-       questText.enabled = true;
+       //animates quest title text to move up 
+       questTextAnim.enabled = true;
 
        yield return new WaitForSeconds(1);
        
